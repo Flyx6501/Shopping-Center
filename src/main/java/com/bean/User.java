05ca5a0eb84ece954d8c4d7e1715f3220a291f1e
L666888999 <1410009393.@qq.com>
2022-11-07 18:17:01 +0800

    [Upload] 上传用户 User 对应的 Bean、Dao、Service、Servlet

---
+++ src/main/java/com/bean/User.java
@@ -0,0 +1,83 @@
+package com.bean;
+
+
+public class User {
+    private int useId;
+    private String userUsername;
+    private String password;
+    private String email;
+    private String address;
+    private String nickname;
+
+    public User() {
+    }
+
+    public User(int useId, String userUsername, String userPassword, String email, String address, String nickname) {
+        this.useId = useId;
+        this.userUsername = userUsername;
+        this.password = userPassword;
+        this.email = email;
+        this.address = address;
+        this.nickname = nickname;
+    }
+
+    public int getUseId() {
+        return useId;
+    }
+
+    public void setUseId(int useId) {
+        this.useId = useId;
+    }
+
+    public String getUserUsername() {
+        return userUsername;
+    }
+
+    public void setUserUsername(String userUsername) {
+        this.userUsername = userUsername;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "useId=" + useId +
+                ", userUsername='" + userUsername + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
+}
