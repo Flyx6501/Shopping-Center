fa417d217e05cea0d800da46584d8fb6b0c96c42
L666888999 <1410009393.@qq.com>
2022-11-08 17:33:14 +0800

    [Update] 更新了用户类 User 的 dao

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -2,26 +2,33 @@ package com.dao;
 
 import com.mysql.jdbc.Connection;
 
+/**
+ * @author l666888999
+ * @version 1.0
+ * @date 2022/11/8 15:10
+ **/
 public interface UserDao {
 
+    /** 登陆时查看出入的用户和密码是否与数据库中的相匹配
+     * @param c JDBC链接
+     * @param userId 用户的ID
+     * @param password 用户密码
+     * @return boolean
+     * @author l666888999
+     * @date 2022/11/07 22:37
+     **/
     boolean searchName(Connection c, int userId, String password);
-/** 判断用户名是否已注册过
-1.前端传变量：用户名username
-2.根据username，查数据库中是否存在用户，其用户名为username
-3.若存在，返回该用户对象 User
-4.若不存在，返回null
-SELECT * FROM xxx WHERE username = username;
-* **/
 
-    /** 校验用户名密码是否匹配
-     * 1.前端传变量：用户名username，密码password
-     * 2.根据传的username、password，查数据库中用户名密码是否匹配
-     * 3.若匹配，返回该用户对象
-     * 4.若不匹配，返回null
-     * SELECT * FROM xxx WHERE username=username AND password=password;
-     * **/
 
-    boolean registerName(Connection c, int userId,String password);
+    /** 注册用户id和密码
+     * @param c JDBC 连接
+     * @param userId 用户ID
+     * @param password 用户密码
+     * @return boolean
+     * @author l666888999
+     * @date 2022/11/08 17:31
+     **/
+    boolean registerName(Connection c, int userId, String password);
 
 }
 
