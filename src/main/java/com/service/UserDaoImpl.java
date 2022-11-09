595c5b446e9fabac97328cfe56920d00ecb9c964
L666888999 <1410009393.@qq.com>
2022-11-09 19:39:26 +0800

    [Update] 更新用户 Service

--- src/main/java/com/service/UserDaoImpl.java
+++ src/main/java/com/service/UserDaoImpl.java
@@ -18,14 +18,14 @@ public class UserDaoImpl implements UserDao {
 
 
     @Override
-    public boolean searchName(Connection c, int userId, String password) {
+    public boolean searchName(Connection c, String userName, String password) {
         PreparedStatement ps = null;
         ResultSet rs = null;
 
         try {
-            String sql = "SELECT user_id,user_password FROM `user` WHERE  user_id=? and user_password=? ";
+            String sql = "SELECT user_username,user_password FROM `user` WHERE  user_username=? and user_password=?";
             ps = c.prepareStatement(sql);
-            ps.setInt(1, userId);
+            ps.setString(1, userName);
             ps.setString(2, password);
             rs = ps.executeQuery();
             while (rs.next()) {
@@ -39,20 +39,17 @@ public class UserDaoImpl implements UserDao {
         return false;
     }
 
-
     @Override
-    public boolean registerName(Connection c, int userId, String password) {
+    public boolean registerName(Connection c,String password, String email, String nickName, String userName) {
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            String sql = "INSERT INTO user(user_name,nickname,user_password,user_password,email) VALUES (?,?,?,?,?)";
+            String sql = "INSERT INTO user(user_username,nickname,user_password,email) VALUES (?,?,?,?)";
             ps = c.prepareStatement(sql);
-            //ps.setInt(1,userId);
-            ps.setString(1, password);
-            ps.setString(2, "");
-            ps.setString(3, "");
-            ps.setString(4, "");
-            ps.setString(5, "");
+            ps.setString(1,userName);
+            ps.setString(2,nickName);
+            ps.setString(3, password);
+            ps.setString(4, email);
 
             int rs1 = ps.executeUpdate();
             if (rs1 >= 1) {
