ad1dc5eb4c9d8c2626de4ff11eb4b7f182ba5809
L666888999 <1410009393.@qq.com>
2022-11-10 11:09:45 +0800

    [Update] 更新了管理员类的Dao

--- src/main/java/com/dao/ManagerDao.java
+++ src/main/java/com/dao/ManagerDao.java
@@ -11,13 +11,13 @@ public interface ManagerDao {
 
     /**  进行管理员登陆
      * @param c 链接数据库
-     * @param manageId  管理员的ID
+     * @param manageName  管理员的name
      * @param password  管理员的密码
      * @return boolean
      * @author l666888999
      * @date 2022/11/8 15:26
      **/
-    boolean searchName(Connection c,int manageId,String password);
+    boolean searchName(Connection c,String manageName,String password);
 
     /** 进行管理员注册
      * @param c  进行数据库链接
