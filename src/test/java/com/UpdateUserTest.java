e6dfc900d7f4beff4fc761927f154e4deaabc6b9
L666888999 <1410009393.@qq.com>
2022-11-12 19:32:55 +0800

    [Update] 更新了对用户修改的Test

---
+++ src/test/java/com/UpdateUserTest.java
@@ -0,0 +1,38 @@
+package com;
+
+import com.service.ManagerDaoImpl;
+import com.utils.JDBCUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 更新用户信息
+ *
+ * @author l666888999
+ * @version 1.0
+ * @date 2022/11/12 14:25
+ **/
+public class UpdateUserTest {
+
+    @Test
+    public void UpdateUsers() {
+        Connection c = null;
+        try {
+            ManagerDaoImpl test = new ManagerDaoImpl();
+            c = JDBCUtil.getConnection();
+            boolean flag = test.updateUserByUserId((com.mysql.jdbc.Connection) c, "ljy", "123", "6161@qq.com", "ll", 2);
+            System.out.println(flag);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
