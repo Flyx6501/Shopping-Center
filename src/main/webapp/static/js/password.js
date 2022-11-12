39eee45912f6c1d63b7ff49b8fc17ee208b78d82
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2022-11-12 11:14:58 +0800

    [Create] 新建了个人中心-修改密码验证

---
+++ src/main/webapp/static/js/password.js
@@ -0,0 +1,94 @@
+/* @description: 个人主页-修改密码
+   @author LL
+   @date 2022/11/12 11:06
+   @version 1.0
+*/
+$(document).ready(function() {
+    $("#oldpassword").blur(function() {
+        var oldPassword = $("#oldPassword").val();
+        if(oldPassword == ''){
+            showMsg("请输入原密码");
+        }
+        else{
+            /* 判断原密码是否正确 */
+            $.ajax({
+                url: "#",
+                data: {
+                    oldpassword:oldPassword
+                },
+                success: function(e) {
+                    if (e.code == 1) {
+                        showMsg("√");
+                    } else {
+                        showMsg("原密码输入错误");
+                    }
+                }
+            });
+        }
+    });
+    $("#firstPassword").blur(function() {
+        var num = $("#firstPassword").val().length;
+        if (num < 6) {
+            showMsg("密码过短");
+        } else if (num > 10) {
+            showMsg("密码过长");
+        } else {
+            showMsg("√");
+        }
+    });
+    $("#secondPassword").blur(function() {
+        var firstPassword = $("#firstPassword").val();
+        var secondPassword = $("#secondPassword").val().length;
+        if ($("#secondPassword").val() != firstPassword) {
+            showMsg("与新密码不相同");
+        } else {
+            if (num >= 6 && num <= 10) {
+                showMsg("√");
+            } else {
+                showMsg("密码不符合规则，密码长度应为6-10个字母或数字");
+            }
+        }
+    });
+    $("#btn").click(function() {
+        var flag = true;
+        var oldpassword = $("#oldpassword").val();
+        var firstPassword = $("#firstPassword").val();
+        var secondPassword = $("#secondPassword").val();
+        var first = $("#firstPassword").val().length;
+        var second = $("#secondPassword").val().length;
+        if (first != second || first < 6 || second < 6 || first > 10 || second > 10 || firstPassword !=
+            secondPassword) {
+            flag = false;
+        } else {
+            flag = true;
+        }
+        if (flag) {
+            /* 修改密码 */
+            $.ajax({
+                url: "#",
+                data: {
+                    oldpassword: old,
+                    password: pass
+                },
+                /* 成功则清空输入框 */
+                success: function(e) {
+                    if (e.code == 1) {
+                        $("#oldpassword").val("");
+                        $("#firstPassword").val("");
+                        $("#secondPassword").val("");
+                        $("#oldPassword").empty();
+                        $("#firstPassword").empty();
+                        $("#secondPassword").empty();
+                        alert("修改成功，请重新登录");
+                        window.location.href="login.jsp";
+                    }
+                }
+            });
+        }
+    });
+    //错误信息提醒
+    function showMsg(msg) {
+        $("#checkMsg").text(msg);
+    }
+});
+
