3bee81f2cc6754d6baf44c7d7037e684e65924d4
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2022-11-14 21:14:29 +0800

    [Update] 更新了主页用户名获取值

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -4,10 +4,16 @@
    @version 1.0
 */
 /* 搜索按钮点击事件 跳转到分类页*/
-function checkSearch(search){
+function checkSearch(search) {
     /* Sid为查询的内容 */
-    window.location.href="result.html?Sid="+$("#find").val();
+    window.location.href = "result.html?Sid=" + $("#find").val();
 }
+
+function details(id) {
+    /* Cid为商品id */
+    window.location.href = "details.do?Cid=" + id;
+}
+
 window.onload = function() {
     $.ajax({
         /* 获取数据 */
@@ -16,34 +22,41 @@ window.onload = function() {
         dataType: "json",
         success: function(data) {
             let products = $("#products");
-            /*商品列表*/
-            var list = data.commodity;
-            console.log(list[1]);
-            /*每四个商品一行*/
-            for (let i = 0; i < Math.ceil(list.length / 4); i++) {
+            var list = data.commodity; //商品列表
+            console.log(list);
+            for (let i = 0; i < Math.ceil(list.length / 4); i++) { //每四个商品一行
                 products.append(`<div class="layui-row" id="list` + i + `"></div>`);
             }
 
             let k = 0;
             for (let i = 0; i < Math.ceil(list.length / 4); i++) {
                 let domRow = $("#list" + i);
+                /* let id = list[k].commodityId; */
                 for (let j = 0; j < 4; j++) {
                     domRow.append(
-                        `<div class="layui-col-md3">
+                        `<div class="layui-col-md3" name="" id="` + list[k].commodityId + `">
 							<div class="thumbnail">
-							<a href="">
-								<img src="" alt="商品图片">
+								<img src="` +list[k].commodityImg+ `" alt="...">
 								<div class="caption">
+								<a href="" onclick="details(` + list[k].commodityId + `)">
 									<h3>` + list[k].commodityName + `</h3>
 										<p>` + list[k].commodityPrice + `</p>
 									</a>
 								</div>
 							</div>
 						</div>`
-                    );
+                );
                     k += 1;
                 }
             }
+        }
+    });
+$.ajax({
+        url: "#",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            $("#userName").html(data.userName);
         }
     });
 }
