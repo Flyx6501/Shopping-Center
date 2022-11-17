f73534d7516cf2156001b4b1c650e4f3ac1b8fc6
Qgs123 <2313039289@qq.com>
2022-11-17 10:44:09 +0800

    [Update] 修改注解

--- src/main/java/com/service/CommodityServiceImpl.java
+++ src/main/java/com/service/CommodityServiceImpl.java
@@ -2,11 +2,9 @@ package com.service;
 
 import com.bean.Commodity;
 import com.dao.CommodityDao;
-
 import java.util.List;
-
-/**
- * @author 23130
+/**商品业务逻辑层
+ * @author Qgs123
  * @version 1.0
  * @date 2022/11/11 10:48
  **/
@@ -14,10 +12,8 @@ public class CommodityServiceImpl implements CommodityService{
     CommodityDao dao=new CommodityDaoImpl();
     @Override
     public List<Commodity> getCommodityList(){
-
         return dao.getCommodityList();
     }
-
     @Override
     public Commodity getOne(int id) {
         return dao.getOne(id);
