d60d860fc6e2310164ee92f9b6abff54de36d66c
L666888999 <1410009393.@qq.com>
2022-11-19 21:09:53 +0800

    [Create] 创建了用户修改密码的Servlet

---
+++ src/main/java/com/servlet/UpdatePassword/UpdatePasswordByUserName.java
@@ -0,0 +1,74 @@
+package com.servlet.UpdatePassword;
+
+import com.mysql.jdbc.Connection;
+import com.service.UserDaoImpl;
+import com.utils.JDBCUtil;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.SQLException;
+
+/**
+ * 进行用户密码更新
+ *
+ * @author l666888999
+ * @version 1.0
+ * @date 2022/11/18 11:49
+ **/
+public class UpdatePasswordByUserName extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        doPost(req,resp);
+        try {
+            updatePasswordByUserName(req,resp);
+        } catch (SQLException  | ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        String methodName = servletPath.substring(1);
+        methodName = methodName.substring(0, methodName.length() - 3);
+
+        Method method = null;
+        try {
+            method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            method.invoke(this, req, resp);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void updatePasswordByUserName(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException, ClassNotFoundException {
+        Connection c= (Connection) JDBCUtil.getConnection();
+        String password = req.getParameter("password");
+        String userName = req.getParameter("userName");
+        UserDaoImpl userDao = new UserDaoImpl();
+        boolean flag = userDao.updateUserByPassword(c, password, userName);
+        System.out.println(password);
+        System.out.println(userName);
+        if (flag){
+            JSONObject json = new JSONObject();
+            json.put("msg","success");
+            PrintWriter out=resp.getWriter();
+            out.println(json);
+            out.close();
+        }
+
+
+    }
+}
