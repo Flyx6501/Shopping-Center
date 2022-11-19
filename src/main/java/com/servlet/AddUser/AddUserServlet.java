cce85ce2ca5b954aa6dfbe8918ef0b0012fdd168
L666888999 <1410009393.@qq.com>
2022-11-19 22:25:39 +0800

    [Create] 创建了增加用户的Servlet

---
+++ src/main/java/com/servlet/AddUser/AddUserServlet.java
@@ -0,0 +1,76 @@
+package com.servlet.AddUser;
+
+import com.mysql.jdbc.Connection;
+import com.service.ManagerDaoImpl;
+import com.utils.JDBCUtil;
+import org.json.JSONObject;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.SQLException;
+
+/**
+ * 添加用户
+ *
+ * @author l666888999
+ * @version 1.0
+ * @date 2022/11/19 21:53
+ **/
+public class AddUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doPost(req,resp);
+        try {
+            addUser(req,resp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        String methodName = servletPath.substring(1);
+        methodName = methodName.substring(0, methodName.length() - 3);
+
+        Method method = null;
+        try {
+            method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            method.invoke(this, req, resp);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void addUser(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException, ClassNotFoundException {
+        String userName = req.getParameter("username");
+        String email = req.getParameter("email");
+        String address = req.getParameter("city");
+        String nickname = req.getParameter("nickname");
+        Connection c= (Connection) JDBCUtil.getConnection();
+        ManagerDaoImpl dao=new ManagerDaoImpl();
+        boolean flag = dao.addUser(c, userName, email, address, nickname);
+        if (flag){
+            JSONObject json=new JSONObject();
+            json.put("msg","增加成功");
+
+            PrintWriter out=resp.getWriter();
+            out.println(json);
+            out.close();
+        }
+
+    }
+}
