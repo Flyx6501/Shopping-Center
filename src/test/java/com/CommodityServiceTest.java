275dbdbd3c319046c752cd1297fc951b4a67151c
Qgs123 <2313039289@qq.com>
2022-11-20 22:25:14 +0800

    [Update] 更新了商品列表测试

--- src/test/java/com/CommodityServiceTest.java
+++ src/test/java/com/CommodityServiceTest.java
@@ -1,24 +1,40 @@
 package com;
 
 import com.bean.Commodity;
-import com.mysql.jdbc.Connection;
+import com.service.CommodityDaoImpl;
 import com.service.CommodityServiceImpl;
+import java.sql.Connection;
+import com.utils.JDBCUtil;
 import org.junit.Test;
-
+import java.sql.SQLException;
 import java.util.List;
 
-/**
- * @author 23130
+/**商品列表
+ * @author Qgs123
  * @version 1.0
  * @date 2022/11/12 19:55
  **/
 public class CommodityServiceTest {
+     /** 查询商品列表
+      * @author Qgs123
+      * @date 2022/11/20 16:51
+      **/
     @Test
     public void getCommodityListTest() {
         CommodityServiceImpl commodityDao = new CommodityServiceImpl();
 
         List<Commodity> list = commodityDao.getCommodityList();
         System.out.println(list);
-
     }
+     /** 获取某个商品的详情
+     * @author Qgs123
+     * @date 2022/11/20 16:53
+      **/
+    @Test
+    public void getOne() {
+        CommodityServiceImpl commodityDao = new CommodityServiceImpl();
+        Commodity list1=commodityDao.getOne(5);
+        System.out.println(list1);
+    }
+
 }
