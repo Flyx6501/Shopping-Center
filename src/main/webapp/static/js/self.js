e9eb2e6517fdc63058fc58b2f21f6d0de053dd40
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2022-11-21 23:17:38 +0800

    [Update] 修改了个人信息修改操作

--- src/main/webapp/static/js/self.js
+++ src/main/webapp/static/js/self.js
@@ -5,12 +5,12 @@
 */
 $(document).ready(function() {
     /* 昵称 */
-    $("#nickName").blur(function() {
-        var num = $("#nickName").val().length;
-        if (num != 11) {
-            showMsg("电话有误");
-        }
-    });
+    /*    $("#nickName").blur(function() {
+            var num = $("#nickName").val().length;
+            if (num != 11) {
+                showMsg("电话有误");
+            }
+        });*/
     /* 邮箱 */
     $("#email").blur(function() {
         var reg = /^\w+@\w+(.\w+)+$/;
@@ -22,24 +22,34 @@ $(document).ready(function() {
     });
 
 
-    $("#submit").on("click", function() {
-        var model = {
-            userName: $("#userName").val(),
+    $("#submit").click(function(){
+
+        /*var model = {
+            userName: $("#selfName").val(),
             nickName: $("#nickName").val(),
             email: $("#email").val(),
             address: $("#address").val(),
-        };
+            userNames:$("#userNames").val(),
+        };*/
         $.ajax({
             /* 传入数据接口 */
-            url: "#",
+            url: "userUpdate.do",
             type: "post",
-            data: model,
+            data: {
+                userName: userName,
+                nickName: $("#nickName").val(),
+                email: $("#email").val(),
+                address: $("#address").val(),
+                userNames:userName,
+            },
             dataType: "json",
             success: function(data) {
+                console.log(123);
                 showMsg("修改成功!");
                 location.reload();
             },
             error: function() {
+                console.log(123);
                 showMsg("修改失败!")
             }
 
