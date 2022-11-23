06fc710969e198dc07b323f6863270096be08551
L666888999 <1410009393.@qq.com>
2022-11-23 08:49:06 +0800

    [Create]创建了查询用户Servlet

---
+++ src/main/java/com/servlet/GetUser/GetAll.java
@@ -0,0 +1,73 @@
+package com.servlet.GetUser;
+
+import com.bean.User;
+import com.service.ManagerDaoImpl;
+import com.utils.JDBCUtil;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 遍历用户信息
+ *
+ * @author l666888999
+ * @version 1.0
+ * @date 2022/11/22 19:54
+ **/
+public class GetAll extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        try {
+            getAll(req,resp);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String servletPath = req.getServletPath();
+        String methodName = servletPath.substring(1);
+        methodName = methodName.substring(0, methodName.length() - 3);
+
+        Method method = null;
+        try {
+            method = getClass().getDeclaredMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            method.invoke(this, req, resp);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void getAll(HttpServletRequest req, HttpServletResponse resp) throws SQLException, IOException, ClassNotFoundException {
+        Connection c = JDBCUtil.getConnection();
+        ManagerDaoImpl dao=new ManagerDaoImpl();
+        List<User> list = dao.selectUser((com.mysql.jdbc.Connection) c);
+        JSONObject json=new JSONObject();
+        json.put("msg",list);
+
+        PrintWriter out=resp.getWriter();
+        out.println(json);
+        out.close();
+        //req.setAttribute("userList", list);
+
+    }
+
+}
