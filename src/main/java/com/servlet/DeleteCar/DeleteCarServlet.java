594bd65895e41c735ddd867ac96468723443f37a
Qgs123 <2313039289@qq.com>
2022-11-29 08:50:52 +0800

    [Update]  更新了删除购物车商品的servlet

--- src/main/java/com/servlet/DeleteCar/DeleteCarServlet.java
+++ src/main/java/com/servlet/DeleteCar/DeleteCarServlet.java
@@ -1,12 +1,17 @@
 package com.servlet.DeleteCar;
 
 import com.bean.Car;
+import com.bean.CarOperate;
+import com.bean.Commodity;
+import com.utils.CommodityNotFindException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 /**删除购物车商品操作
  * @author Qgs123
@@ -21,27 +26,27 @@ public class DeleteCarServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /**拿到购物车
-        */
-         List<Car> car=(List<Car>)req.getSession().getAttribute("car");
+         */
+        CarOperate car = new CarOperate((Map<String, Integer>) req.getSession().getAttribute("car"));
         /**先接收到id
-        */
+         */
         int id=Integer.parseInt(req.getParameter("id"));
-        Car i=null;
+        Commodity i=new Commodity();
         /**遍历购物车
-        */
-        for (Car c : car) {
-            if(Objects.equals(c.getCommodity().getCommodityId(), id)) i = c;
+         */
+        for (String commodityId : car.getCar().keySet()) {
+            if(Objects.equals(commodityId, id)) {
+                try {
+                    car.remove(commodityId);
+                } catch (CommodityNotFindException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
-        /**将商品删除
-        */
-        car.remove(i);
         /**遍历购物车将总价加上
-        */
-         int sum=0;
-        for (Car car2 : car) {
-            sum+=car2.getCarCommodityTotal();
-        }
-        req.getSession().setAttribute("sum",sum);
+         */
+        Double priceCount = car.getCarPriceCount();
+        req.getSession().setAttribute("sum",priceCount);
         resp.sendRedirect("car.jsp");
     }
 }
