02ed26dc047210154b5b7afa110764e7bc90df23
Qgs123 <2313039289@qq.com>
2022-11-29 08:52:12 +0800

    [Update]  更新了修改购物车商品数量操作的servlet

--- src/main/java/com/servlet/UpdateCar/UpdateCarServlet.java
+++ src/main/java/com/servlet/UpdateCar/UpdateCarServlet.java
@@ -1,8 +1,11 @@
 package com.servlet.UpdateCar;
 
 import com.bean.Car;
+import com.bean.CarOperate;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,28 +23,26 @@ public class UpdateCarServlet extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         /**拿到购物车
-       */
-        List<Car> car=(List<Car>)req.getSession().getAttribute("car");
+         */
+        CarOperate car = new CarOperate((Map<String, Integer>) req.getSession().getAttribute("car"));
         /**接收到传过来的值
-        */
+         */
         int id=Integer.parseInt(req.getParameter("id"));
         int type=Integer.parseInt(req.getParameter("type"));
-        for (Car c : car) {
-            if(Objects.equals(c.getCommodity().getCommodityId(), id)) {
-                int count=c.getCarCommodityCount()+type;
+        for (String cId : car.getCar().keySet()) {
+            if(Objects.equals(cId, id)) {
+                int count=car.getCar().get(cId)+type;
                 /**设置该商品的数量
-               */
-                if(count<1)break;
-                c.setCarCommodityCount(count);
-                c.setCarCommodityTotal(c.getCarCommodityCount()*c.getCommodity().getCommodityPrice());
+                 */
+                if(count<1){
+                    break;}
+                car.add(cId,count);
             }
         }
         /**遍历购物车将总价加上
-        */
-        int sum=0;
-        for (Car car2 : car) {
-            sum+=car2.getCarCommodityTotal();
-        }
+         */
+        Double sum= Double.valueOf(0);
+        sum = car.getCarPriceCount();
         req.getSession().setAttribute("sum",sum);
         resp.sendRedirect("car.jsp");
     }
