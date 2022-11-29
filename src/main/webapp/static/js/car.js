c98607b6bf07fa6e0f1e5c154d54124bfd8c3d8c
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2022-11-29 10:40:17 +0800

    [Update] 修改了购物车页面操作

--- src/main/webapp/static/js/car.js
+++ src/main/webapp/static/js/car.js
@@ -3,88 +3,240 @@
    @date 2022/11/14 10:15
    @version 1.0
 */
-$(function() {
-    // $(".c-sum_num").click(function(){
-    // 	console.info($(this).text());
-    // })
-    var url = window.location.search;
-    var id = url.split("=");
+/* 减数量 */
+function reduce(cid, sum) {
+    /* 最初数量 */
+    var num = $("#commodityNum" + cid).val();
+    if (num > 0) {
+        num--;
+        $("#commodityNum" + cid).val(num);
+    };
+    var price = $("#commodityPrice" + cid).text();
+    sum = parseFloat(price * num);
+    /* 保留一位 */
+    function math() {
+        sum = Math.round(sum * 100) / 100;
+    }
+    math();
+    $("#commoditySum" + cid).text(sum);
+
+    var id = cid;
+    let cname = $("#commodityName" + cid).text();
+    let cnum = $("#commodityNum" + cid).val();
+    $.ajax({
+        url: "#",
+        type: "POST",
+        dataType: "json",
+        data: {
+            commodityId: id,
+            commodityName: cname,
+            commodityNum: cnum,
+            userName: userName,
+        },
+        success: function(data) {
+
+        }
+    });
+    /* totl(); */
+}
+/* 加数量 */
+function add(cid, sum) {
+    var num = $("#commodityNum" + cid).val();
+    num++;
+    $("#commodityNum" + cid).val(num);
+    var price = $("#commodityPrice" + cid).text();
+    sum = parseFloat(price * num);
+
+    function math() {
+        sum = Math.round(sum * 100) / 100;
+    }
+    math();
+    $("#commoditySum" + cid).text(sum);
+    var id = cid;
+    let cname = $("#commodityName" + cid).text();
+    let cnum = $("#commodityNum" + cid).val();
+    /* console.log(cname);
+    console.log(cnum); */
+    $.ajax({
+        url: "#",
+        type: "POST",
+        dataType: "json",
+        data: {
+            commodityId: id,
+            commodityName: cname,
+            commodityNum: cnum,
+            userName: userName,
+        },
+        success: function(data) {
+
+        }
+    });
+}
+
+/* 删除 */
+function del(cid) {
+    $("#good" + cid).remove();
+    $.ajax({
+        url: "#",
+        type: "POST",
+        dataType: "json",
+        data: {
+            commodityId: id,
+            commodityName: cname,
+            commodityNum: cnum,
+            userName: userName,
+        },
+        success: function(data) {
+
+        }
+    });
+}
+/*检查复选框*/
+function checkboxOnclick(cid) {
+    let sum = 0;
+    if ($("#check" + cid).val() == 0) {
+        sum += parseFloat($("#commoditySum" + cid).text());
+        let money = $("#money").text(); //取
+        let moneys = parseFloat(money);
+        moneys += sum; //算
+        $("#money").text(moneys); //放
+        $("#check" + cid).val(1);
+    } else {
+        var money = $("#money").text(); //取
+        var moneys = parseFloat(money);
+        sum = $("#commoditySum" + cid).text();
+        var sums = parseFloat(sum);
+        /* var smoney = moneys - sums; */
+        moneys -= sums;
+        moneys = Math.round(moneys * 100) / 100;
+        /*console.log(moneys);*/
+        $("#money").text(moneys);
+        $("#check" + cid).val(0);
+        /* console.log($("#check" + cid).val()); */
+    }
+}
+
+/*全选*/
+function changeAll() {
+    let sum = 0;
+    /* val=1表示被选中,val=0表示未被选 */
+    if ($("#all").val() == 0) {
+        for (let i = 1; i <= 2; i++) {
+            sum += parseFloat($("#commoditySum" + i).text());
+        }
+        $("#money").text(sum);
+        /* 所有选框都val为1 */
+        /*console.log(sum);*/
+        $("input[type='checkbox']").val(1);
+        /*console.log($("#all").val());*/
+        /* $("#all").val(1); */
+    } else {
+        /* 反选 即在被选中时（val=1时）再次点击，值变为0*/
+        $("#money").text(0);
+        $("input[type='checkbox']").val(0);
+        /* $("#all").val(0); */
+    }
+}
+
+/* 被选中的放入购物车 */
+function checkSome() {
+    for (var i = 0; i <= 2; i++) {
+        if ($("#check" + i).val() == 0) {
+            continue;
+        }
+        var orders = new Array(2);
+        for (var i = 0; i < 2; i++) {
+            let j=i+1;
+            var order = {};
+            order["oid"] = j;
+            order["cid"] = $("#commodityId" + j).val();
+            /* order["userName"] = userName; */
+            order["cnum"] = $("#commodityNum" + j).val();
+
+            orders[i] = order;
+            var json = JSON.stringify(orders);
+        }
+        /*console.log(json);*/
+    }
+
+    $.ajax({
+        url: "#",
+        type: "post",
+        dataType: "json",
+        data: {
+            json,
+        },
+        success: function(data) {
+
+        }
+    });
+
+}
+
+window.onload = function() {
+    let sum = 0;
     /* 返回值给购物车 */
     $.ajax({
-        /* 获取数据 */
-        url: "car.do",
+        /* 获取全部数据 */
+        url: "#",
         type: "GET",
         dataType: "json",
         data: {
-            "id": id,
+             userName: userName,
         },
-        success: function(data) {
+        success: function(commodity) {
             /* 读取成功时将数据读取,显示在页面内 */
-            $("#img").html(data.commodityImg);
-            $("#goodsName").html(data.commodityName);
-            $("#props").html(data.commodityInformation);
-            $("#price").html(data.commodityPrice);
-            /* 定位 遍历 */
+            let commodityName = $("#commodityName").val;
+            let car = $("#car");
+            /* 获取购物车列表 */
+            let k = 0;
+            for (k = 0; k < 2; k++) {
+                let cid = commodity[k].commodityId;
+                let cname = commodity[k].commodityName;
+                let cinformation = commodity[k].commodityInformation;
+                let cprice = commodity[k].commodityPrice;
+                let cnum = commodity[k].commodityNum;
+                let csum = commodity[k].commoditySum;
+                /* 初始总价 */
+                sum = parseFloat(cprice * cnum);
+                car.append(
+                    `<div class="carts-goods" id="good` + cid + `">
+					<input class="input" type="hidden" name="commodityId" id="commodityId` + cid + `" value="` + cid + `"> 
+					<div class="cell c-select">
+						<div class="c-checkbox">
+							<input type="checkbox" name="check" class="each" value="0" id="check` + cid +
+                    `" onclick="checkboxOnclick(` + cid + `)">
+						</div>
+					</div>
+					<div class="cell c-goodsname" id ="commodityName` + cid + `">` + cname + `</div>
+					<div class="cell c-props" id ="commodityInformation` + cid + `">` + cinformation + `</div>
+					<div class="cell c-price">
+						<span>￥</span>
+						<div class="c-pricenum" id ="commodityPrice` + cid + `">` + cprice + `</div>
+					</div>
+					<div class="cell c-quantity">
+						<button type="button" class="reduce" id="reduce` + cid + `" onclick="reduce(` + cid + `)">-</button>
+						<input type="text" id ="commodityNum` + cid + `" value="` + cnum + `" class="text-num">
+						<button type="button" class="add" id="add` + cid + `" onclick="add(` + cid + `)">+</button>
+					</div>
+					<div class="cell c-sum">
+						<span>￥</span>
+						<div class="c-sumnum" id ="commoditySum` + cid + `">` + sum + `</div>
+					</div>
+					<div class="cell c-action">
+						<a href="#" id="remove" class="remove" onclick="del(` + cid + `)">移除商品</a>
+					</div>
+				</div>`
+                );
+            }
         }
     });
-    totl();
-    /* goodsnum(); */
-    // 全选
-    $(".all").click(function() {
+    /* 全选 */
+    $("#all").click(function(checkbox) {
         all = $(this).prop("checked")
         $(".each").each(function() {
             $(this).prop("checked", all);
         })
-    })
-    // 减少商品数量
-    $(".reduce").click(function() {
-        var num = $(this).siblings(".text-num").val();
-        if (num > 0) {
-            num--;
-            $(this).siblings(".text-num").val(num);
-        };
-        var price = $(this).parents().siblings(".c-price").children(".c-pricenum").text();
-        var sum_num = $(this).parents().siblings(".c-sum").children(".c-sumnum");
-        var sum = parseFloat(price * num);
-        // console.log(sum);
-        $(sum_num).text(sum);
-        totl();
-         /* goodsnum(); */
+        changeAll();
     });
-    // 增加商品数量
-    $(".add").click(function() {
-        var num = $(this).siblings(".text-num").val();
-        num++;
-        $(this).siblings(".text-num").val(num);
-        var price = $(this).parents().siblings(".c-price").children(".c-pricenum").text();
-        var sum_num = $(this).parents().siblings(".c-sum").children(".c-sumnum");
-        var sum = parseFloat(price * num);
-        // console.log(sum);
-        $(sum_num).text(sum);
-        totl();
-        /* goodsnum(); */
-    });
-    // 删除商品
-    $(".remove").click(function() {
-        $(this).parents(".carts-goods").remove();
-        totl();
-        /* goodsnum(); */
-    });
-    // 总价
-    function totl() {
-        let sumprice = 0;
-        $.each($(".c-sumnum"), function() {
-            sumprice += parseFloat($(this).text());
-            $(".show-money span").text(sumprice);
-        });
-    };
-    // 统计商品数量
-    /* function goodsnum() {
-        let goods_num = 0;
-        $.each($(".text-num"), function() {
-            goods_num += parseInt($(this).val());
-            $(".goods_num span").text(goods_num);
-        });
-    } */
-
-});
+}
