9ff07f85d7f134cd1ff6a3927f6910c5070eb737
xhmm1126 <1257857566@qq.com>
2022-12-01 11:06:01 +0800

    [Update] 更新了新增商品信息后自动关闭弹窗及提示新增成功的功能

--- src/main/webapp/static/js/commodityAdd.js
+++ src/main/webapp/static/js/commodityAdd.js
@@ -11,22 +11,26 @@ layui.use(['form', 'laydate'], function() {
 
     //监听提交
     form.on("submit(save)", function(data) {
-        console.log(data, 'data');
-        var index = layer.load();
+        // console.log(data, 'data');
         $.ajax({
             url: baseUrl+'/addCommodity.do?commodityName='+data.field.commodityName+'&commodityPrice='+data.field.commodityPrice+'&commodityStock='+data.field.commodityStock+'&commodityInformation='+data.field.commodityInformation,
             type: "post",
             contentType: "application/json;charset=UTF-8",
             // data: JSON.stringify(data.field),
             dataType: 'json',
+            async: false,
             success: function(res) {
-                console.log(res);
+                //console.log(res);
                 if (res.msg == 200) {
-                    // 提示新增成功
-                    layer.msg("新增成功！");
-                    layer.close(index);
-                    // 关闭弹窗页面
-                    // 刷新表格
+                    layer.msg("新增成功", {
+                        icon: 1,
+                        time: 2000 //2秒关闭（如果不配置，默认是3秒）
+                    }, function(){
+                        //do something
+                        window.parent.location.reload();//刷新父页面
+                        var index = parent.layer.getFrameIndex(window.name);
+                        parent.layer.close(index);
+                    });
 
                 } else {
                     layer.msg("新增失败，请重试！");
