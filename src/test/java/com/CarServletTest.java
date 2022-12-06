25e205dbd4de3f74bb8c5e84bc9dc895c0d3cd64
Qgs123 <2313039289@qq.com>
2022-12-06 15:52:15 +0800

    [Create]  新建了购物车的测试类

---
+++ src/test/java/com/CarServletTest.java
@@ -0,0 +1,35 @@
+package com;
+
+import com.bean.Car;
+import com.service.CarDaoImpl;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**π∫ŒÔ≥µ≤‚ ‘¿‡
+ * @author Qgs123
+ * @version 1.0
+ * @date 2022/12/1 21:13
+ **/
+public class CarServletTest {
+    @Test
+    public void GetCarByUserIdTest(){
+        CarDaoImpl carDao=new CarDaoImpl();
+        List<Map> car=carDao.getCarByUserId("1");
+        System.out.println(car);
+    }
+    @Test
+    public void UpdateCommodityTest(){
+        CarDaoImpl carDao=new CarDaoImpl();
+        Integer car=carDao.updateCommodity(59,"1",1,2);
+        System.out.println(car);
+    }
+    @Test
+    public void DeleteCommodity(){
+        CarDaoImpl carDao=new CarDaoImpl();
+        Integer car=carDao.deleteCommodity("1",1);
+        System.out.println(car);
+    }
+
+}
