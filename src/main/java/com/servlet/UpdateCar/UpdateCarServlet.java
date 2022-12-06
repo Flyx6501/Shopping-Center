0dcd06e651034f87e58a22f9b6f68bc7eec39716
Qgs123 <2313039289@qq.com>
2022-12-06 15:46:55 +0800

    [Update]  更新了修改购物车商品数量操作Servlet

--- src/main/java/com/servlet/UpdateCar/UpdateCarServlet.java
+++ src/main/java/com/servlet/UpdateCar/UpdateCarServlet.java
@@ -2,8 +2,15 @@ package com.servlet.UpdateCar;
 
 import com.bean.Car;
 import com.bean.CarOperate;
+import com.dao.CarDao;
+import com.service.CarDaoImpl;
+import com.utils.CommodityNotFindException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -16,34 +23,60 @@ import javax.servlet.http.HttpServletResponse;
  * @date 2022/11/16 22:08
  **/
 public class UpdateCarServlet extends HttpServlet{
+    CarDao carDao =new CarDaoImpl();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        /**拿到购物车
+        /**拿到购物车,先将传过来的id接收到
          */
-        CarOperate car = new CarOperate((Map<String, Integer>) req.getSession().getAttribute("car"));
-        /**接收到传过来的值
-         */
-        int id=Integer.parseInt(req.getParameter("id"));
-        int type=Integer.parseInt(req.getParameter("type"));
-        for (String cId : car.getCar().keySet()) {
-            if(Objects.equals(cId, id)) {
-                int count=car.getCar().get(cId)+type;
-                /**设置该商品的数量
-                 */
-                if(count<1){
-                    break;}
-                car.add(cId,count);
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter wirte = null;
+        int result =-1;
+        Map resultMap = new HashMap<>();
+        String userId=String.valueOf(req.getParameter("userId"));
+        List<Map> car= carDao.getCarByUserId(userId);
+
+        Integer commodityId = Integer.valueOf(req.getParameter("commodityId"));
+        Integer commodityNum = Integer.valueOf(req.getParameter("commodityNum"));
+
+        Double priceTotal = Double.valueOf(0);
+        for(Map commodity : car){
+            priceTotal = priceTotal + Integer.parseInt(commodity.get("car_commodity_num").toString()) * Double.parseDouble(commodity.get("commodity_price").toString());
+            /**购物车中有该商品
+            */
+            if(commodity.get("car_commodity_id").equals(commodityId)){
+                Integer carId =  Integer.parseInt(commodity.get("car_id").toString());
+                int commodityNumNew = commodityNum;
+                result = carDao.updateCommodity(carId,userId,commodityId,commodityNumNew);
+                priceTotal += commodityNum * Double.parseDouble(commodity.get("commodity_price").toString());
+            }else{
+                /**购物车中没有该商品
+                */
+                result = carDao.addCommodity(userId,commodityId,commodityNum);
+                Map commodityAdd = carDao.getCommodityById(String.valueOf(commodityId));
+                priceTotal += commodityNum * Double.parseDouble(commodityAdd.get("commodity_price").toString());
             }
+
         }
-        /**遍历购物车将总价加上
+        resultMap.put("priceTotal",priceTotal);
+        /**重定向网页
          */
-        Double sum= Double.valueOf(0);
-        sum = car.getCarPriceCount();
-        req.getSession().setAttribute("sum",sum);
-        resp.sendRedirect("car.jsp");
+        if(result<0){
+            resultMap.put("result","error");
+        }else{
+            resultMap.put("result","success");
+        }
+        /**声明JSONArray对象并输入JSON字符串
+        */
+         JSONObject json=new JSONObject();
+        json.put("result",resultMap);
+        PrintWriter out=resp.getWriter();
+        out.println(json);
+        out.close();
     }
 }
