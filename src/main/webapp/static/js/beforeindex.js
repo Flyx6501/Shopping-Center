ed3944a7526c29c1e1e0bf25579055698f82d7aa
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2023-01-17 00:11:51 +0800

    [Create] 新增登录前首页操作

---
+++ src/main/webapp/static/js/beforeindex.js
@@ -0,0 +1,74 @@
+/* @description: 前台首页验证
+   @author LL
+   @date 2022/11/12 17:36
+   @version 1.0
+*/
+/* 搜索按钮点击事件 跳转到分类页*/
+checkSearch = function () {
+    /* Sid为查询的内容 */
+    let url ="beforeresult?Sid=" + $("#find").val();
+    window.location.href = url;
+}
+clothes = function (){
+    let url ="beforesort?Sname=" + $("#clothes").text();
+    window.location.href = url;
+}
+shoes = function (){
+    let url ="beforesort?Sname=" + $("#shoes").text();
+    window.location.href = url;
+}
+food = function (){
+    let url ="beforesort?Sname=" + $("#food").text();
+    window.location.href = url;
+}
+good = function (){
+    let url ="beforesort?Sname=" + $("#good").text();
+    window.location.href = url;
+}
+electronics = function (){
+    let url ="beforesort?Sname=" + $("#electronics").text();
+    window.location.href = url;
+}
+
+window.onload = function() {
+    $.ajax({
+        /* 获取数据 */
+        url: "index.do", //获取商品列表
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            let products = $("#products");
+            /*console.log(data);*/
+            var list = data.commodity; //商品列表
+            /*console.log(list);*/
+            for (let i = 0; i < Math.ceil(list.length / 4); i++) { //每四个商品一行
+                products.append(`<div class="layui-row" id="list` + i + `"></div>`);
+            }
+
+            let k = 0;
+            for (let i = 0; i < Math.ceil(list.length / 4); i++) {
+                let domRow = $("#list" + i);
+                /* let id = list[k].commodityId; */
+                for (let j = 0; j < 4; j++) {
+                    if (k >= list.length)
+                        break;
+                    domRow.append(
+                        `<div class="layui-col-md3" name="" id="` + list[k].commodityId + `">
+							<div class="thumbnail">
+								<img src="` +list[k].commodityPhoto+ `" alt="...">
+								<div class="caption">
+								<!--<a href="#" onclick="details()">-->
+							<a href="beforedetails?id=` + list[k].commodityId +`">
+									<h3>` + list[k].commodityName + `</h3>
+										<p>` + list[k].commodityPrice + `</p>
+									</a>
+								</div>
+							</div>
+						</div>`
+                    );
+                    k += 1;
+                }
+            }
+        }
+    });
+}
