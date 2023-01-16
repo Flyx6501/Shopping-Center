a416d33062ab0ca0fc87f7e63bb028f48d4f9981
Bule Tomato <113242161+youyudexihongshi@users.noreply.github.com>
2023-01-17 00:08:53 +0800

    [Update] 修改分类页面操作

--- src/main/webapp/static/js/sort.js
+++ src/main/webapp/static/js/sort.js
@@ -3,16 +3,28 @@
    @date 2022/11/14 10:08
    @version 1.0
 */
+var url = window.location.search;
+var sname = url.split("=%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20");
+//console.log(sname);
+//console.log(sname[1]);
+var name = decodeURI(sname[1]);
+//console.log(name);
 window.onload = function() {
+    $("#sortName").text(name);
+    let commodityName = $("#sortName").text();
+    //console.log(commodityName);
     $.ajax({
         /* 获取数据 */
-        url: "index.do",
+        url: "getFuzzyQuery.do",
         type: "GET",
         dataType: "json",
+        data:{
+          commodityName:commodityName,
+        },
         success: function(data) {
             let products = $("#products");
             var list = data.commodity;
-            /*console.log(list);*/
+            //console.log(list);
             /*遍历显示*/
             for (let i = 0; i < Math.ceil(list.length / 4); i++) {
                 products.append(`<div class="layui-row" id="list` + i + `"></div>`);
@@ -22,12 +34,11 @@ window.onload = function() {
                 let domRow = $("#list" + i);
                 for (let j = 0; j < 4; j++) {
                     domRow.append(
-                        `<div class="layui-col-md3">
+                        `<div class="layui-col-md3" name="" id="` + list[k].commodityId + `">
 							<div class="thumbnail">
-								<img src="" alt="...">
+								<img src="` +list[k].commodityPhoto+ `" alt="...">
 								<div class="caption">
-								<a href="">
-								<img src="" alt="商品图片">
+							<a href="details?id=` + list[k].commodityId +`">
 									<h3>` + list[k].commodityName + `</h3>
 										<p>` + list[k].commodityPrice + `</p>
 									</a>
